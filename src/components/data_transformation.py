5130982fc47dfe709dc9c625783695e04a93f184
Bansari Panseriya <bansaripanseriya010@gmail.com>
2024-04-08 16:20:22 +0530

    Model Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -99,6 +99,7 @@ class DataTransformation:
                 f"Applying preprocessing object on training dataframe and testing dataframe."
             )
 
+            ## CALLING .PKL FILE AND PERFORM fit AND transformation FUNCTION ON IT
             input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
